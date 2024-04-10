32c271f4e07c4cdc28b20cbb956b5b6f5bbc9321
Brian Monestime <bmonestime@gmail.com>
2024-04-09 18:26:20 -0600

    Complete

---
+++ nameEntry/src/App.jsx
@@ -0,0 +1,60 @@
+import "./App.css";
+import React, { useState } from "react";
+
+function App() {
+  //setting state
+  const [formData, setFormData] = useState({
+    input: "",
+    namesList: [],
+  });
+
+  //submit event
+  function handleClick(event) {
+    event.preventDefault();
+    setFormData((prevInfo) => ({
+      ...prevInfo,
+      namesList: [...prevInfo.namesList, formData.input],
+    }));
+    //logic to clear input after submit
+    setFormData((prevInfo) => ({
+      ...prevInfo,
+      input: "",
+    }));
+  }
+  // input on change
+  function handleChange(event) {
+    event.preventDefault();
+    const { name, value } = event.target;
+
+    setFormData((prevName) => {
+      //spreading previous data into the object
+      //event.target.name: event.target.value
+      return { ...prevName, [name]: value };
+    });
+  }
+
+  return (
+    <div>
+      <h1>{formData.input}</h1>
+      <ol>
+        {formData.namesList.map((data, index) => {
+          return <li key={index}>{data}</li>;
+        })}
+      </ol>
+
+      <input
+        type="text"
+        onChange={handleChange}
+        name="input"
+        value={formData.input}
+      />
+      <button onClick={handleClick}>Submit</button>
+      <br></br>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
